50c67bbcca759ce67ad122ea62d376e286ec7a32
Sawyer Timperley <sawyertimperley@gmail.com>
2020-04-25 15:32:25 -0500

    echo request with outside network

--- ClientSocket.py
+++ ClientSocket.py
@@ -1,16 +1,20 @@
 import socket
+import time
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 10000
+UDP_IP = "73.37.174.96"
+UDP_PORT = 9652
 
 client_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_conn.bind((UDP_IP, UDP_PORT))
 
 print("Listening...")
 # TODO do this with a configurable delay period
 while True:
     encode_string = ""
-    data = client_conn.recvfrom(1024)
-    print(data[0].decode())
+    data = client_conn.sendto(str.encode('ping'), (UDP_IP, UDP_PORT))
+
+    rcv_data = client_conn.recvfrom(1024)
+    send_time = int(str(rcv_data[0].decode()).split()[1])
+    diff = (time.time_ns() - send_time) * 1000
+    print(f"time difference from sender: {diff}ms")
 
     #TODO add timeout after no received messages
--- ServerSocket.py
+++ ServerSocket.py
@@ -3,23 +3,28 @@ import time
 
 # test variables
 MESSAGE = "Hello World!"
-UDP_PORT = 10000
-UDP_IP = "127.0.0.1"  # use the localhost loopback for now
+UDP_PORT = 9652
+UDP_IP = "192.168.1.124"  # use the localhost loopback for now
 
 # establish socket object. SOCK_DGRAM indicates a UDP data frame, stored in a static context.
 conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+conn.bind(('', UDP_PORT))
 
 # wait for connections on the port. If some are found, send data to them.
 # This must be sent as a bytestream, objects don't work here.
 
 # TODO make this with a configurable delay period
-print("Deploying Server...")
+print(f"Deploying Server on {UDP_IP}:{UDP_PORT}")
 counter = 0
 # get the time since the server started
+
+
 while True:
+    d = conn.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
     counter += 1
     delta = time.time_ns()
-    data = str(counter) + " " + str(delta)
-    conn.sendto(str.encode(data), (UDP_IP, UDP_PORT))
-
+    timestamp_msg = str(counter) + " " + str(delta)
+    conn.sendto(str.encode(timestamp_msg), addr)
     time.sleep(1)
